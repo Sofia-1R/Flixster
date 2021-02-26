9797936e2861417f0d59f69bb5b4d5695064ee8d
sofia10R <sofiafifi10@gmail.com>
2021-02-25 20:56:09 -0500

    'done with part two'

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,24 +1,33 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     Context context;
@@ -52,6 +61,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -63,6 +73,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview= itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
 
         }
@@ -82,8 +93,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             }
 
 
-
+            int radius = 10;
+            int margin = 0;
+           // Glide.with(context).load(imageUrl).;
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //Register click listener on whole row
+            //Action take by tapping navigates to new activity
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,31 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
+    int movieId;
     String posterPath;
     String backDropPath;
     String title;
     String overview;
+    double rating;
+    double popular;
 
+    public Movie() {
+        //empty constructor needed by Parcel
+    }
     public Movie(JSONObject jsonObject) throws JSONException {
         backDropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview= jsonObject.getString("overview");
-
+        rating= jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+        popular = jsonObject.getDouble("popularity");
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
         List<Movie> movies = new ArrayList<>();
@@ -40,5 +49,17 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getPopular() {
+        return Double.toString(popular);
     }
 }
